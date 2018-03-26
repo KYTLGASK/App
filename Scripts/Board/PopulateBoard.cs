aa7be12bb8b1b541f4d622e17deb0cea9e512817
KYTL GASK <KYTL.GASK@gmail.com>
2018-03-26 15:36:34 +0300

    Inital tile commit - added Tile prefab, board + scripts for: a) populating board. b) highlighting and un-highlighting tiles (courtey of Leon)

---
+++ Scripts/Board/PopulateBoard.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateBoard : MonoBehaviour {
+    
+    public int gridX;
+    public int gridY;
+    public float spacingW; //Width spacing (Y axis)
+    public float spacingL; //Length spacing (X axis)
+
+    public GameObject tile;
+    public Camera FirstPersonCam, ThirdPersonCam;
+    public KeyCode TKey;
+    public bool camSwitch = false;
+
+
+
+    private float height;
+    private float width;   
+    private float length;
+
+    private float initialX = 0f;
+    private float initialY = 0f;
+
+    private float x;
+    private float y;
+
+    private GameObject currentTile;
+
+	// Use this for initialization
+	void Start () {
+
+        height = tile.transform.localScale.y;
+        width = tile.transform.localScale.z;
+        length = tile.transform.localScale.x;
+
+        float sumX = gridX * length + (gridX - 1) * spacingL;
+        initialX = (initialX - sumX / 2) + length / 2;
+
+        float sumY = gridY * width + (gridY - 1) * spacingW;
+        initialY = (initialY - sumY / 2) + width / 2;
+
+
+        x = initialX;
+        y = initialY;
+
+        for (int i=0; i < gridY; i++)
+        {
+            for (int j=0; j < gridX; j++)
+            {
+                currentTile = (GameObject)Instantiate(tile, new Vector3(x, height/2, y), Quaternion.identity, transform);
+                currentTile.name = i + ", "+j; //row,column
+                x += length + spacingL;
+            }
+            y += (width + spacingW); // (+) if initializing board from bottom up, (-) otherwise 
+            x = initialX;
+        }
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(TKey))
+        {
+            camSwitch = !camSwitch;
+            FirstPersonCam.gameObject.SetActive(camSwitch);
+            ThirdPersonCam.gameObject.SetActive(!camSwitch);
+        }
+    }
+}
