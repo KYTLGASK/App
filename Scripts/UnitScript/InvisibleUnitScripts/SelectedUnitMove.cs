cb50762dd84c51590d7cdc5697620c8e24e5e6ea
KYTL GASK <KYTL.GASK@gmail.com>
2018-03-27 17:58:35 +0300

    Cleared everything that doesn't belong here.

--- Scripts/UnitScript/InvisibleUnitScripts/SelectedUnitMove.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// this is the "invisible unit which stores last clicked unit"
-public class SelectedUnitMove : MonoBehaviour {
-    public string CurrUnitName;//last clicked unit's object name
-    public bool isSelected = false;//is a unit currently selected
-}
